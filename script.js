e2b14d902ff254a5c837c4d3a2e18877b22714c4
MR Laboratory <58909573+mrlaboratory@users.noreply.github.com>
2021-09-18 12:08:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -23,15 +23,47 @@ blogStats.child("value").set(data.value);
 }); 
 }); 
 
+// Download click counter firebase
+$.each($("a[name]"), function(i, e) { 
+var elem = $(e).parent().find("#downloadview"); 
+var blogStats = new Firebase("https://bloggerpv-2575a-default-rtdb.firebaseio.com/pages/id/" + $(e).attr("name")); 
+blogStats.once("value", function(snapshot) { 
+var data = snapshot.val(); 
+var isnew = false; 
+if(data == null) { 
+data= {}; 
+data.value = 0; 
+data.url = window.location.href; 
+data.id = $(e).attr("name"); 
+isnew = true; 
+} 
+elem.text(data.value); 
+}); 
+}); 
+function downloadcount(){
+    $.each($("a[name]"), function(i, e) { 
+var elem = $(e).parent().find("#downloadview"); 
+var blogStats = new Firebase("https://bloggerpv-2575a-default-rtdb.firebaseio.com/pages/id/" + $(e).attr("name")); 
 
+blogStats.once("value", function(snapshot) { 
+var data = snapshot.val(); 
+var isnew = false; 
+if(data == null) { 
+data= {}; 
+data.value = 0; 
+data.url = window.location.href; 
+data.id = $(e).attr("name"); 
+isnew = true; 
+} 
+elem.text(data.value); 
+data.value++; 
+if(window.location.pathname!="/") { 
+if(isnew) 
+blogStats.set(data); 
+else 
+blogStats.child("value").set(data.value); 
 
-// $( ".container .card" ).mouseover(function() {
-//     $(".details").fadeIn('slow');
-//   });
-
-//   $( ".container .card" ).mouseout(function() {
-//     $(".details").fadeOut('slow');
-//   });
+}}); }); }
 
 function copyaurl(){
   $(".sharediv .url button").text("Copyed");
