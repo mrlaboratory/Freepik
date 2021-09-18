f247b39e19cf8ccd5244473cadfcddf4f5d5fa0a
MR Laboratory <58909573+mrlaboratory@users.noreply.github.com>
2021-09-18 12:07:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-// alert("worked");
+// Post View Counter 
+$.each($("a[name]"), function(i, e) { 
+var elem = $(e).parent().find("#postviews"); 
+var blogStats = new Firebase("https://bloggerpv-2575a-default-rtdb.firebaseio.com/pages/id/" + $(e).attr("name")); 
+blogStats.once("value", function(snapshot) { 
+var data = snapshot.val(); 
+var isnew = false; 
+if(data == null) { 
+data= {}; 
+data.value = 0; 
+data.url = window.location.href; 
+data.id = $(e).attr("name"); 
+isnew = true; 
+} 
+elem.text(data.value); 
+data.value++; 
+if(window.location.pathname!="/") { 
+if(isnew) 
+blogStats.set(data); 
+else 
+blogStats.child("value").set(data.value); 
+} 
+}); 
+}); 
+
 
 
 // $( ".container .card" ).mouseover(function() {
